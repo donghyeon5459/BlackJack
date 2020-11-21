a3e1e13562eaf878d9039d13bf024ed104590278
DONGHYEON <kevink3@naver.com>
2020-11-21 21:49:17 +0900

    main.c

--- main.c
+++ main.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <netdb.h>
+#include <signal.h>
+#include <unistd.h>
+
+#include <pthread.h>
+#include "server_user.h"
 
 int main(void)
 {
