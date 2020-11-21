f1a4416328cfd97f77fc39985d000f67ad26267a
DONGHYEON <kevink3@naver.com>
2020-11-21 21:59:59 +0900

    make socklib

---
+++ socklib.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define HOSTLEN 256
+#define BACKLOG 1
