0546ba9f1fcb27068fecc30b991e9dad328e9027
DONGHYEON <kevink3@naver.com>
2020-11-21 21:52:25 +0900

    main 수정

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/wait.h>
@@ -9,7 +8,6 @@
 #include <netdb.h>
 #include <signal.h>
 #include <unistd.h>
-
 #include <pthread.h>
 #include "server_user.h"
 
